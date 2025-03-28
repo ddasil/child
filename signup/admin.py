af9cc2881bf49a558c286bc35e772f0cecc8ce13
princessria <princessria@naver.com>
2025-03-28 11:26:24 +0900

    회원가입 데이터 저장

--- signup/admin.py
+++ signup/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-# Register your models here.
+# Django 기본 User 모델 등록
+# admin.site.register(User)
--- signup/views.py
+++ signup/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
-# Create your views here.
-#add1
 def goSignup(request):
-    return render(request, 'signup.html')
-#add2
+    if request.method == "POST":
+        username = request.POST.get("userid")
+        password = request.POST.get("pwd1")
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "이미 존재하는 아이디입니다.")
+            return render(request, "signup.html")
+
+        # User 모델에 저장
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+
+        messages.success(request, "회원가입이 완료되었습니다.")
+        return redirect("login")  # 🔥 여기서 로그인 페이지로 이동!
+    
+    return render(request, "signup.html")
